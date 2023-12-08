9d7e8a81df379df64d55f646f803cc4f6ce897f9
Yasir <yasir.fayrooz@preezie.com>
2023-12-08 16:47:32 +1100

    doing exercise 1 WIP

---
+++ 1. Hello World/1.1 Comments/hello.rs
@@ -0,0 +1,16 @@
+
+
+fn main(){
+    //Line comments which go to the end of line
+    /* 
+    Block comment
+    Which spans multiple blocks
+     */
+
+    ///Generate library docs for the following item
+    /// 
+    println!("Hello World!");
+
+    //! Generate library docs for the enclosing item
+    //! 
+}
---
+++ 1. Hello World/hello.rs
@@ -0,0 +1,6 @@
+
+
+fn main(){
+    //Test Comment
+    println!("Hello World!");
+}
